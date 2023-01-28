26ebcfa46fde115da8ab9de3c37c1bda619ace86
Rafael Lefever <rafael.lefever@discomco.pl>
2023-01-28 06:14:36 +0100

    updated push.sh

--- push.sh
+++ push.sh
@@ -2,9 +2,18 @@
 clear
 
 echo '-----------------------------------'
-echo pushing version "$1" to dev branch
+echo 'pushing version "$1" to dev branch'
 echo '-----------------------------------'
 git add .
 git commit -m "$1" -a
 git push  
-
+echo '-----------------------------------'
+echo 'pushing version "$1" to master branch'
+echo '-----------------------------------'
+git checkout master
+git merge dev
+git push
+echo '-----------------------------------'
+echo 'back to dev'
+echo '-----------------------------------'
+git checkout dev
